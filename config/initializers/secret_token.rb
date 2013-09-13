9eb53b639551722136ba10dd6b383e48013ef245
drjoss <g.gablaya@gmail.com>
2013-09-13 16:49:57 +0300

    Agent pre Build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agents::Application.config.secret_token = '3e9373b3332aedfd25db84ca5108e401dda3625d4d46b34dc97ebf6bcb0cf7b68e067002e033938bacdbe3d891ee32f0c418df7477068c41d5295e968b56e555'
